5e29a82b855f90bc5c4d1fadde45d99559f5b031
komazsofi <komazsofi@gmail.com>
2019-09-30 14:35:26 +0200

    preprocess diff scales

--- 1_Pipeline_PreProcess.R
+++ 1_Pipeline_PreProcess.R
@@ -8,45 +8,41 @@ library("lidR")
 library("rgdal")
 
 # Set working directory
-workingdirectory="D:/Koma/Paper1_v2/ALS/" ## set this directory where your input las files are located
+workingdirectory="D:/Koma/Paper1/Revision/input/process/" ## set this directory where your input las files are located
 #workingdirectory="D:/Koma/Paper1/ALS/"
 setwd(workingdirectory)
 
 cores=18
 chunksize=2000
-buffer=2.5
-resolution=2.5
+buffer=1
+resolution=1
 
 rasterOptions(maxmemory = 200000000000)
+
+library(future)
+plan(multisession, workers = 4L)
+set_lidr_threads(4L)
 
 # Create catalog 
 ctg <- catalog(workingdirectory)
 
-# Classify ground
+# Normalize with point neighborhood
 
 opt_chunk_buffer(ctg) <- buffer
 opt_chunk_size(ctg) <- chunksize
 opt_cores(ctg) <- cores
-opt_output_files(ctg) <- paste(workingdirectory,"ground/{XLEFT}_{YBOTTOM}_gr",sep="")
+opt_output_files(ctg) <- paste(workingdirectory,"normalized_neibased/{XLEFT}_{YBOTTOM}_gr_norm",sep="")
 
-ground_ctg <- lasground(ctg, pmf(2.5,0.1))
-
-# Normalize with point neighborhood
-
-opt_chunk_buffer(ground_ctg) <- buffer
-opt_chunk_size(ground_ctg) <- chunksize
-opt_cores(ground_ctg) <- cores
-opt_output_files(ground_ctg) <- paste(workingdirectory,"normalized_neibased/{XLEFT}_{YBOTTOM}_gr_norm",sep="")
-
-normalized_ctg=lasnormalize(ground_ctg,knnidw(k=20,p=2))
+normalized_ctg=lasnormalize(ctg,knnidw(k=20,p=2))
 
 # Generate DTM
 
-opt_output_files(ground_ctg)=""
+opt_chunk_size(ctg) <- chunksize
+opt_output_files(ctg)=""
 
-dtm = grid_terrain(ground_ctg, algorithm =knnidw(k=20,p=2), res=2.5, keep_lowest = TRUE)
+dtm = grid_terrain(ctg, algorithm =knnidw(k=20,p=2), res=1, keep_lowest = TRUE)
 crs(dtm) <- "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs"
-writeRaster(dtm, "dtm.tif",overwrite=TRUE)
+writeRaster(dtm, "dtm_1.tif",overwrite=TRUE)
 
 # Hillshade
 
@@ -54,30 +50,4 @@ slope <- terrain(dtm, opt='slope')
 aspect <- terrain(dtm, opt='aspect')
 dtm_shd <- hillShade(slope, aspect, 40, 270)
 
-writeRaster(dtm_shd, "dtm_shd.tif",overwrite=TRUE)
-
-# Create DSM
-
-dsm = grid_metrics(ground_ctg,max(Z),res=2.5)
-crs(dsm) <- "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs"
-writeRaster(dsm, "dsm.tif",overwrite=TRUE)
-
-# Hillshade
-
-slope <- terrain(dsm, opt='slope')
-aspect <- terrain(dsm, opt='aspect')
-dsm_shd <- hillShade(slope, aspect, 40, 270)
-
-writeRaster(dsm_shd, "dsm_shd.tif",overwrite=TRUE)
-
-# Homogenization
-normalized_ctg <- catalog(paste(workingdirectory,"normalized_neibased/",sep=""))
-opt_output_files(normalized_ctg) <- paste(workingdirectory,"homogenized/{XLEFT}_{YBOTTOM}_gr_norm_homo",sep="")
-
-homogenized_ctg=lasfilterdecimate(normalized_ctg,homogenize(12,1))
-
-# Homogenization without normalization
-normalized_ctg2 <- catalog(paste(workingdirectory,"ground/",sep=""))
-opt_output_files(normalized_ctg2) <- paste(workingdirectory,"ground_homogenized/{XLEFT}_{YBOTTOM}_gr_homo",sep="")
-
-homogenized_ctg2=lasfilterdecimate(normalized_ctg2,homogenize(12,1))
+writeRaster(dtm_shd, "dtm_shd_1.tif",overwrite=TRUE)
--- 2_Pipeline_LiDARmetrics.R
+++ 2_Pipeline_LiDARmetrics.R
@@ -6,68 +6,56 @@ Aim: This script is aimed to extract the liDAR metrics every possible way (with 
 # Import required R packages
 library("lidR")
 library("rgdal")
-source("D:/Koma/GitHub/myPhD_escience_analysis/Paper1_inR_v2/Function_LiDARMetricsCalc.R") #set where the Function*.R file located
+source("D:/Koma/GitHub/PhDPaper1_Classifying_wetland_habitats/Function_LiDARMetricsCalc.R") #set where the Function*.R file located
 #source("D:/GitHub/eEcoLiDAR/myPhD_escience_analysis//Paper1_inR_v2/Function_LiDARMetricsCalc.R")
 
 # Set working directory
-workingdirectory="D:/Koma/Paper1_v2/ALS/" ## set this directory where your input las files are located
+workingdirectory="D:/Koma/Paper1/Revision/input/process/" ## set this directory where your input las files are located
 #workingdirectory="D:/Koma/Paper1/ALS/"
 setwd(workingdirectory)
 
 cores=18
 chunksize=2000
-buffer=2.5
+buffer=1
 resolution=2.5
 
 rasterOptions(maxmemory = 200000000000)
 
 ### Ground run
 
-ground_ctg <- catalog(paste(workingdirectory,"ground/",sep=""))
+ground_ctg <- catalog(workingdirectory)
 
 opt_chunk_buffer(ground_ctg) <- buffer
 opt_chunk_size(ground_ctg) <- chunksize
 opt_cores(ground_ctg) <- cores
 
+library(future)
+plan(multisession, workers = 6L)
+set_lidr_threads(6L)
+
 # Calculate metrics
 
 covermetrics = grid_metrics(ground_ctg,  CoverageMetrics(Z,Classification), res = resolution)
 #plot(covermetrics)
-writeRaster(covermetrics,"covermetrics_gr.grd",overwrite=TRUE)
+writeRaster(covermetrics,paste("covermetrics_gr_",resolution,"m.grd",sep=""),overwrite=TRUE)
 
 shapemetrics = grid_metrics(ground_ctg,  EigenMetrics(X,Y,Z), res = resolution)
 #plot(shapemetrics)
-writeRaster(shapemetrics,"shapemetrics_gr.grd",overwrite=TRUE)
+writeRaster(shapemetrics,paste("shapemetrics_gr_",resolution,"m.grd",sep=""),overwrite=TRUE)
 
 vertdistr_metrics = grid_metrics(ground_ctg, VertDistr_Metrics(Z),res=resolution)
 #plot(vertdistr_metrics)
-writeRaster(vertdistr_metrics,"vertdistr_metrics_gr.grd",overwrite=TRUE)
+writeRaster(vertdistr_metrics,paste("vertdistr_metrics_gr_",resolution,"m.grd",sep=""),overwrite=TRUE)
 
 height_metrics = grid_metrics(ground_ctg, HeightMetrics(Z),res=resolution)
 #plot(height_metrics)
-writeRaster(height_metrics,"height_metrics_gr.grd",overwrite=TRUE)
+writeRaster(height_metrics,paste("height_metrics_gr_",resolution,"m.grd",sep=""),overwrite=TRUE)
 
 proj4string(height_metrics)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
 horizontal_metrics = HorizontalMetrics(height_metrics$zmax)
 #plot(horizontal_metrics)
-writeRaster(horizontal_metrics,"horizontal_metrics_gr.grd",overwrite=TRUE)
-
-# Only for vegetation
-
-opt_filter(ground_ctg) <- "-keep_class 1"
-
-shapemetrics_whgr = grid_metrics(ground_ctg,  EigenMetrics(X,Y,Z), res = resolution)
-#plot(shapemetrics_whgr)
-writeRaster(shapemetrics_whgr,"shapemetrics_whgr_gr.grd",overwrite=TRUE)
-
-vertdistr_metrics_whgr = grid_metrics(ground_ctg, VertDistr_Metrics(Z),res=resolution)
-#plot(vertdistr_metrics_whgr)
-writeRaster(vertdistr_metrics_whgr,"vertdistr_metrics_whgr_gr.grd",overwrite=TRUE)
-
-height_metrics_whgr = grid_metrics(ground_ctg, HeightMetrics(Z),res=resolution)
-#plot(height_metrics)
-writeRaster(height_metrics_whgr ,"height_metrics_whgr_gr.grd",overwrite=TRUE)
+writeRaster(horizontal_metrics,paste("horizontal_metrics_gr_",resolution,"m.grd",sep=""),overwrite=TRUE)
 
 ### Normlized-ground run
 
@@ -79,23 +67,7 @@ opt_cores(normalized_ctg) <- cores
 
 covermetrics = grid_metrics(normalized_ctg,  CoverageMetrics(Z,Classification), res = resolution)
 #plot(covermetrics)
-writeRaster(covermetrics,"covermetrics_gr_norm.grd",overwrite=TRUE)
-
-shapemetrics = grid_metrics(normalized_ctg,  EigenMetrics(X,Y,Z), res = resolution)
-#plot(shapemetrics)
-writeRaster(shapemetrics,"shapemetrics_gr_norm.grd",overwrite=TRUE)
-
-vertdistr_metrics = grid_metrics(normalized_ctg, VertDistr_Metrics(Z),res=resolution)
-#plot(vertdistr_metrics)
-writeRaster(vertdistr_metrics,"vertdistr_metrics_gr_norm.grd",overwrite=TRUE)
-
-height_metrics = grid_metrics(normalized_ctg, HeightMetrics(Z),res=resolution)
-#plot(height_metrics)
-writeRaster(height_metrics,"height_metrics_gr_norm.grd",overwrite=TRUE)
-
-horizontal_metrics = HorizontalMetrics(height_metrics$zmax)
-#plot(horizontal_metrics)
-writeRaster(horizontal_metrics,"horizontal_metrics_gr_norm.grd",overwrite=TRUE)
+writeRaster(covermetrics,paste("covermetrics_gr_norm_",resolution,"m.grd",sep=""),overwrite=TRUE)
 
 # Only for vegetation
 
@@ -103,89 +75,18 @@ opt_filter(normalized_ctg) <- "-keep_class 1"
 
 shapemetrics_whgr = grid_metrics(normalized_ctg,  EigenMetrics(X,Y,Z), res = resolution)
 #plot(shapemetrics_whgr)
-writeRaster(shapemetrics_whgr,"shapemetrics_whgr_gr_norm.grd",overwrite=TRUE)
+writeRaster(shapemetrics_whgr,paste("shapemetrics_whgr_gr_norm_",resolution,"m.grd",sep=""),overwrite=TRUE)
 
 vertdistr_metrics_whgr = grid_metrics(normalized_ctg, VertDistr_Metrics(Z),res=resolution)
 #plot(vertdistr_metrics_whgr)
-writeRaster(vertdistr_metrics_whgr,"vertdistr_metrics_whgr_gr_norm.grd",overwrite=TRUE)
+writeRaster(vertdistr_metrics_whgr,paste("vertdistr_metrics_whgr_gr_norm_",resolution,"m.grd",sep=""),overwrite=TRUE)
 
 height_metrics_whgr = grid_metrics(normalized_ctg, HeightMetrics(Z),res=resolution)
 #plot(height_metrics)
-writeRaster(height_metrics_whgr ,"height_metrics_whgr_gr_norm.grd",overwrite=TRUE)
+writeRaster(height_metrics_whgr ,paste("height_metrics_whgr_gr_norm_",resolution,"m.grd",sep=""),overwrite=TRUE)
 
 horizontal_metrics_whgr = HorizontalMetrics(height_metrics_whgr$zmax)
 #plot(horizontal_metrics)
-writeRaster(horizontal_metrics_whgr,"horizontal_metrics_whgr_gr_norm.grd",overwrite=TRUE)
+writeRaster(horizontal_metrics_whgr,paste("horizontal_metrics_whgr_gr_norm_",resolution,"m.grd"),overwrite=TRUE)
 
-### Homogenized-normlized-ground run
-
-homo_normalized_ctg <- catalog(paste(workingdirectory,"homogenized/",sep=""))
-
-opt_chunk_buffer(homo_normalized_ctg) <- buffer
-opt_chunk_size(homo_normalized_ctg) <- chunksize
-opt_cores(homo_normalized_ctg) <- cores
-
-shapemetrics = grid_metrics(homo_normalized_ctg,  EigenMetrics(X,Y,Z), res = resolution)
-#plot(shapemetrics)
-writeRaster(shapemetrics,"shapemetrics_gr_norm_homo.grd",overwrite=TRUE)
-
-vertdistr_metrics = grid_metrics(homo_normalized_ctg, VertDistr_Metrics(Z),res=resolution)
-#plot(vertdistr_metrics)
-writeRaster(vertdistr_metrics,"vertdistr_metrics_gr_norm_homo.grd",overwrite=TRUE)
-
-height_metrics = grid_metrics(homo_normalized_ctg, HeightMetrics(Z),res=resolution)
-#plot(height_metrics)
-writeRaster(height_metrics,"height_metrics_gr_norm_homo.grd",overwrite=TRUE)
-
-# Only for vegetation
-
-opt_filter(homo_normalized_ctg) <- "-keep_class 1"
-
-shapemetrics_whgr = grid_metrics(homo_normalized_ctg,  EigenMetrics(X,Y,Z), res = resolution)
-#plot(shapemetrics_whgr)
-writeRaster(shapemetrics_whgr,"shapemetrics_whgr_gr_norm_homo.grd",overwrite=TRUE)
-
-vertdistr_metrics_whgr = grid_metrics(homo_normalized_ctg, VertDistr_Metrics(Z),res=resolution)
-#plot(vertdistr_metrics_whgr)
-writeRaster(vertdistr_metrics_whgr,"vertdistr_metrics_whgr_gr_norm_homo.grd",overwrite=TRUE)
-
-height_metrics_whgr = grid_metrics(homo_normalized_ctg, HeightMetrics(Z),res=resolution)
-#plot(height_metrics)
-writeRaster(height_metrics_whgr ,"height_metrics_whgr_gr_norm_homo.grd",overwrite=TRUE)
-
-### Homogenized-ground run
-
-homo_ctg <- catalog(paste(workingdirectory,"ground_homogenized/",sep=""))
-
-opt_chunk_buffer(homo_ctg) <- buffer
-opt_chunk_size(homo_ctg) <- chunksize
-opt_cores(homo_ctg) <- cores
-
-shapemetrics = grid_metrics(homo_ctg,  EigenMetrics(X,Y,Z), res = resolution)
-#plot(shapemetrics)
-writeRaster(shapemetrics,"shapemetrics_gr_homo.grd",overwrite=TRUE)
-
-vertdistr_metrics = grid_metrics(homo_ctg, VertDistr_Metrics(Z),res=resolution)
-#plot(vertdistr_metrics)
-writeRaster(vertdistr_metrics,"vertdistr_metrics_gr_homo.grd",overwrite=TRUE)
-
-height_metrics = grid_metrics(homo_ctg, HeightMetrics(Z),res=resolution)
-#plot(height_metrics)
-writeRaster(height_metrics,"height_metrics_gr_homo.grd",overwrite=TRUE)
-
-# Only for vegetation
-
-opt_filter(homo_ctg) <- "-keep_class 1"
-
-shapemetrics_whgr = grid_metrics(homo_ctg,  EigenMetrics(X,Y,Z), res = resolution)
-#plot(shapemetrics_whgr)
-writeRaster(shapemetrics_whgr,"shapemetrics_whgr_gr_homo.grd",overwrite=TRUE)
-
-vertdistr_metrics_whgr = grid_metrics(homo_ctg, VertDistr_Metrics(Z),res=resolution)
-#plot(vertdistr_metrics_whgr)
-writeRaster(vertdistr_metrics_whgr,"vertdistr_metrics_whgr_gr_homo.grd",overwrite=TRUE)
-
-height_metrics_whgr = grid_metrics(homo_ctg, HeightMetrics(Z),res=resolution)
-#plot(height_metrics)
-writeRaster(height_metrics_whgr ,"height_metrics_whgr_gr_homo.grd",overwrite=TRUE)
 
